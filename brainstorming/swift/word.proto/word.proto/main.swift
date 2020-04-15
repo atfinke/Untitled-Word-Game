ae3982b24490f6b91d9634a58b9fcde1cac3b024
Andrew Finke <andrewfinke@me.com>
2020-04-15 14:10:58 -0500

    proto init

---
+++ brainstorming/swift/word.proto/word.proto/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  word.proto
+//
+//  Created by Andrew Finke on 4/15/20.
+//  Copyright © 2020 Andrew Finke. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
